64336adf08049016e00b3cc04d91c707aa2d6013
ramon chuinca <ramonchuinca@hotmail.com>
2025-08-18 16:05:50 -0400

    organizando

--- src/views/components/Perfilexterno.vue
+++ src/views/components/Perfilexterno.vue
@@ -17,7 +17,15 @@
   O sistema foi pensado para ser intuitivo, com uma interface simples e moderna, garantindo uma ótima experiência desde o primeiro acesso.
 </p>
 
-
+<div class="flex justify-center gap-4">
+      <!-- Botão Anterior -->
+      <router-link
+        to="/component/Dashbord-externo"
+        class="inline-block bg-gray-500 hover:bg-gray-600 text-white font-medium py-2 px-8 rounded-full transition duration-300"
+        title="Voltar para a etapa anterior"
+      >
+         Anterior
+      </router-link>
     <router-link
       to="/component/Drive-externo"
       class="inline-block bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-8 rounded-full transition duration-300"
@@ -25,6 +33,7 @@
     >
       Próximo 
     </router-link>
+    </div>
   </div>
 </template>
 
